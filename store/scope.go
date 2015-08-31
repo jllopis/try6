12763e7dbeb1b8caddaa699275406a092be46bb8
Joan Llopis <jllopis@acb.es>
2015-08-31 02:00:23 +0200

    store: added GetScopesByTenantID

--- store/scope.go
+++ store/scope.go
@@ -10,6 +10,7 @@ import (
 // Scoper defines the methods needed to manage Tenants
 type Scoper interface {
 	SaveScope(s *try6.Scope) error
+	GetScopesByTenantID(id string) ([]*try6.Scope, error)
 }
 
 // SaveScope persist the scope data to the database
@@ -23,4 +24,16 @@ func (d *DefaultStore) SaveScope(s *try6.Scope) error {
 		return d.C.InsertInto("scopes").Blacklist("id", "deleted").Record(s).Returning("id").QueryScalar(&s.ID)
 	}
 	return d.C.Update("scopes").SetBlacklist(s, "id", "tenant_uid", "created").Where("id=$1", s.ID).Returning("*").QueryStruct(s)
+}
+
+// GetScopesByTenantID returns a list of scopes owned by the tenant or an error
+// if something goes wrong
+func (d *DefaultStore) GetScopesByTenantID(id string) ([]*try6.Scope, error) {
+	log.LogD("Listing Scopes", "pkg", "store", "func", "GetScopesByTenantID(id string)", "tenantID", id)
+	var scopes []*try6.Scope
+	err := d.C.Select("*").From("scopes").Where("tenant_id=$1 AND deleted IS NULL", id).QueryStructs(&scopes)
+	if err != nil {
+		return nil, err
+	}
+	return scopes, nil
 }
